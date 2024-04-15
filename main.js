3defce040e89835524b79bbbd38dbbd120cb1abd
bogdankis <chismbogdan@gmail.com>
2024-04-15 15:22:48 +0300

    added testimonials section

--- main.js
+++ main.js
@@ -70,3 +70,17 @@ function openFullImg(pic){
 function closeFullImg(pic){
     fullImgBox.style.display = "none";
 }
+
+const swiper = new Swiper(".swiper", {
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+
