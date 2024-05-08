67b853471d40c17307ab9de401c866e52d401a7d
bogdankis <chismbogdan@gmail.com>
2024-05-08 22:52:24 +0300

    added form validation to contact form

--- main.js
+++ main.js
@@ -84,7 +84,6 @@ const swiper = new Swiper(".swiper", {
 });
 
 function emailSend() {
-
 	var userName = document.getElementById('name').value;
     var email = document.getElementById('email').value;
 	var phone = document.getElementById('phone').value;
@@ -112,4 +111,66 @@ function emailSend() {
   	}
   }
 );
+}
+
+function validateForm() {
+    if (emailSend()) {  // Validates and attempts to send the email
+      document.getElementById('contactForm').reset();  // Resets the form only if the email was sent
+      return false;  // Prevents the default form submission
+    }
+    return false;  // Prevents the default form submission if validation fails
+  }
+
+  function emailSend() {
+    var userName = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var message = document.getElementById('message').value;
+
+    // Validation checks
+    if (!userName) {
+        swal("Error", "Please enter your name!", "error");
+        return;
+    }
+    if (!email) {
+        swal("Error", "Please enter your email!", "error");
+        return;
+    } else if (!email.match(/^[^@]+@[^@]+\.[^@]+$/)) {
+        swal("Error", "Please enter a valid email address!", "error");
+        return;
+    }
+    if (!phone) {
+        swal("Error", "Please enter your phone number!", "error");
+        return;
+    } else if (!phone.match(/^\d{10}$/)) {
+        swal("Error", "Please enter a valid phone number with exactly 10 digits.", "error");
+        return;
+    }
+    if (!message) {
+        swal("Error", "Please enter a message!", "error");
+        return;
+    }
+
+    var messageBody = "Name: " + userName +
+        "<br/> Email: " + email +
+        "<br/> Phone: " + phone +
+        "<br/> Message: " + message;
+
+    Email.send({
+        Host: "smtp.elasticemail.com",
+        Username: "clouddispose1@gmail.com",
+        Password: "579FA43D94A6D9695545D237D3B6A9FBFC25",
+        To: "clouddispose1@gmail.com",
+        From: "clouddispose1@gmail.com",
+        Subject: "ECOTag Customer",
+        Body: messageBody
+    }).then(
+        message => {
+            if (message === 'OK') {
+                swal("Successful", "Email was sent!", "success");
+            } else {
+                swal("Error", "Email wasn't sent!", "error");
+            }
+        }
+    );
 }
